2b9f43ebd74604bb2fbd62178c9d13486b6ac85a
y0naha <lionafa@gmail.com>
2022-11-08 01:21:51 -0300

    WIP: index sem o banner

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "15b3ebd470a44fa0"; }
+/******/ 		__webpack_require__.h = function() { return "4eb48c323df5fc17"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
@@ -1222,6 +1222,11 @@
 /******/ 		var chunkLoadingGlobal = self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || [];
 /******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
 /******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/nonce */
+/******/ 	!function() {
+/******/ 		__webpack_require__.nc = undefined;
 /******/ 	}();
 /******/ 	
 /************************************************************************/
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,90 @@
+import config from "../config.json";
+import styled from "styled-components";
+import { CSSReset } from "../src/components/CSSReset";
+import Menu from "../src/components/Menu";
+import { StyledTimeline } from "../src/components/Timeline";
+
 function HomePage() {
-    return <div>Welcome to Next.js!</div>
+  const estilosHome = { 
+    // backgroundColor: "red" 
+  };
+
+  // console.log(config.playlists);
+
+  return (
+    <>
+    <CSSReset/>
+    <div style={estilosHome}>
+      <Menu></Menu>
+      <Header></Header>
+      <TimeLine playlists={config.playlists}></TimeLine>
+    </div>
+    </>
+  );
+}
+
+export default HomePage;
+
+// function Menu() {
+//   return <div>Menu</div>;
+// }
+
+const StyledHeader = styled.div`
+  img {
+    width: 80px;
+    height: 80px;
+    border-radius: 50%;
   }
-  
-  export default HomePage
+  .user-info {
+    margin-top: 50px;
+    display: flex;
+    align-items: center;
+    width: 100%;
+    padding: 16px 32px;
+    gap: 16px;
+  }
+`;
+function Header() {
+  return (
+    <StyledHeader>
+      {/* <img src="banner"/> */}
+      <section className="user-info">
+        <img src={`https://github.com/${config.github}.png`} />
+        <div>
+          <h2>{config.nome}</h2>
+          <p>{config.cargo}</p>
+        </div>
+      </section>
+    </StyledHeader>
+  );
+}
+
+function TimeLine(props) {
+  // console.log("Dentro do componente", props.playlists);
+  const playlistNames = Object.keys(props.playlists);
+// Retorno por expressao
+  return (
+    <StyledTimeline>
+        {playlistNames.map((playlistNames) => {
+          const videos = props.playlists[playlistNames];
+          return (
+            <section>
+              <h2>{playlistNames}</h2>
+              <div>
+                {videos.map((video) => {
+              return (
+                <a href={video.url}>
+                  <img src={video.thumb}/>
+                  <span>
+                    {video.title}
+                  </span>
+                </a>
+                  )
+                })}
+              </div>
+            </section>
+          )
+        })}
+    </StyledTimeline>
+  )
+}
